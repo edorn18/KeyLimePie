50a6e3cb6be3751e2491e3f92716e20b625bf130
edorn18 <Erica@pcp073480pcs.wireless.calpoly.edu>
2017-05-24 10:25:18 -0700

    fixed for loop in NewExrpression

--- m2/given_parser/ast/NewExpression.java
+++ m2/given_parser/ast/NewExpression.java
@@ -23,16 +23,12 @@ public class NewExpression
    public Value buildBlock(List<Block> allBlockList, Block curBlock, Block endBlock, Hashtable<String, Type> globalTable, Hashtable<String, Type> localTable, Hashtable<String, String> varTable, List<TypeDeclaration> types) {
       Value r, r2, r3;
       int size = 0;
-System.out.println("id is: " + id);
+
       for (int i = 0; i < types.size(); i++) {
-         System.out.println("type name is: " + types.get(i).getTypeName());
-         if (types.get(i).getTypeName() == id) {
+         if ((types.get(i).getTypeName()).equals(id)) {
             size = types.get(i).getFields().size();
-      System.out.println("size1 is: " + size);
          }
       }
-
-      System.out.println("size2 is: " + size);
 
       CallMallocInstruction instr = new CallMallocInstruction(size); 
       curBlock.addInstruction(instr);
